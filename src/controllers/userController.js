b2b47e28ebea658fd025ebb8674d1fd6244bf2b2
rupeshhardas <rupesh7875804349@gmail.com>
2022-02-28 07:58:51 +0530

    assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,12 +5,12 @@ const UserModel=require("../models/userModel.js")
 
 const CreateBook= async function(req, res) {
     let data = req.body
-    let BooksData= await userModel.create(data)
+    let BooksData= await UserModel.create(data)
     res.send({Books: BooksData}) 
 }
 
 const getBookData = async function (req, res) {
-    let allbooks = await userModel.find()
+    let allbooks = await UserModel.find()
     res.send({Books: allbooks})
 }
 
